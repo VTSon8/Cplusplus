efaa8f8d5aaacd64529a137759c04234c56f5bc3
VTSon8 <anhkhongnho88888@gmail.com>
2022-01-17 21:18:49 +0700

    xay dung ham nhap mang

--- src/Array/Array001.cpp
+++ src/Array/Array001.cpp
@@ -1,14 +1,19 @@
 #include<iostream>
 using namespace std;
 #define MAX 100
+void nhap(int a[],int &n);
 int main(){
     // khai bao
     int a[MAX];
     int n;
+    // nhap mang
+    nhap(a,n);
+    return 0;
+}
+void nhap(int a[],int &n){
     cin>>n;// nhap n
     // nhap cac phan tu mang
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    return 0;
 }
