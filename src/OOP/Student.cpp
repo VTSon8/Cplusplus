85e0603e8771c8069d9085b3d28aca3462313422
VTSon8 <anhkhongnho88888@gmail.com>
2022-01-17 22:14:21 +0700

    them phuong thuc input va output

--- src/OOP/Student.cpp
+++ src/OOP/Student.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std;
 class Student{
     // du lieu
     private:
@@ -11,3 +13,28 @@ class Student{
         void input();
         void output();
 };
+void Student::input(){
+    cout<<"ID: ";
+    cin>>id;
+    cout<<"Name: ";
+    cin>>name;
+    cout<<"Math: ";
+    cin>>math;
+    cout<<"Physical: ";
+    cin>>physical;
+    cout<<"Chemistry: ";
+    cin>>chemistry;
+}
+void Student::output(){
+    cout<<"ID:"<<id;
+    cout<<"Name: "<<name;
+    cout<<"Math: "<<math;
+    cout<<"Physical: "<<physical;
+    cout<<"Chemistry: "<<chemistry;
+}
+int main(){
+    Student son;
+    son.input();
+    son.output();
+    return 0;
+}
