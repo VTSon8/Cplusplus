ab7631f2e91eaf7d67164bd9aca274d737b51636
VTSon8 <anhkhongnho88888@gmail.com>
2022-01-17 21:11:07 +0700

    nhap mang

---
+++ src/Array/Array001.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+#define MAX 100
+int main(){
+    // khai bao
+    int a[MAX];
+    int n;
+    cin>>n;// nhap n
+    // nhap cac phan tu mang
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    return 0;
+}
