96ab22b3937393b309944759a2e5c7789de1f4b5
VTSon8 <anhkhongnho88888@gmail.com>
2022-01-17 22:04:19 +0700

    khai bao class Student

---
+++ src/OOP/Student.cpp
@@ -0,0 +1,13 @@
+class Student{
+    // du lieu
+    private:
+        string id;
+        string name;
+        float math;
+        float physical;
+        float chemistry;
+    // hanh vi
+    public:
+        void input();
+        void output();
+};
