be514575b3bde51df2d9db68f6f48468f91437cc
VTSon8 <anhkhongnho88888@gmail.com>
2022-01-18 09:58:00 +0700

    Khoi tao co doi so

--- src/OOP/CPoint.cpp
+++ src/OOP/CPoint.cpp
@@ -3,12 +3,18 @@ class CPoint{
         float x,y;
     public:
         CPoint();// khoi tao mac dinh
+        CPoint(float x, float y);
 };
 CPoint::CPoint(){
-    this->x = 1;
-    this->y = 1;
+    this->x = 0;
+    this->y = 0;
+}
+CPoint::CPoint(float x, float y){
+    this->x = x;
+    this->y = y;
 }
 int main(){
     CPoint point1;// tao doi tuong
+    CPoint point2 = CPoint(3,4);// tao doi tuong
     return 0;
 }
