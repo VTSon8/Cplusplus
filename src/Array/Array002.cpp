2dc18d55b57527d880599b1013417c736d28b502
VTSon8 <anhkhongnho88888@gmail.com>
2022-01-19 21:21:47 +0700

    Liet ke so chinh phuong

--- src/Array/Array002.cpp
+++ src/Array/Array002.cpp
@@ -1,29 +1,45 @@
 //nhap vao 1 mang va in ra so chinh phuong
+// so chinh phuong 4 9 16 ...
 #include<iostream>
+#include<cmath>
 using namespace std;
 #define MAX 50
 void nhapMang(int a[],int &n);// khai bao ham nhap
 void xuatMang(int a[],int &n);// khai bao ham xuat
+bool laSoChinhPhuong(int a);// khai bao ham kiem tra so chinh phuong
+void lietKeSoChinhPhuong(int a[],int n); // khai bao ham liet ke so chinh phuong
 int main(){
     int mang[MAX];
     int n;
-    nhapMang(mang,n);
-    xuatMang(mang,n);
+    nhapMang(mang,n);// goi ham nhap mang
+    xuatMang(mang,n);// goi ham xuat mang
+    lietKeSoChinhPhuong(mang,n);
     return 0;
 }
-void nhapMang(int a[],int &n){// dinh nghia ham nhap
+void nhapMang(int a[],int &n){// xay dung ham nhap
     // nhap vao so luong phan tu
     cout<<"n = ";
     cin>>n;
     // nhap vao cac gia tri mang
     for(int i=0;i<n;i++){
-        cout<<"Phan tu thu"<<i+1<<": ";
+        cout<<"Phan tu thu "<<i+1<<": ";
         cin>>a[i];
     }
 }
-void xuatMang(int a[],int &n){//  dinh nghia ham xuat
+void xuatMang(int a[],int &n){//  xay dung ham xuat
     for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
+    }
+    cout<<endl;
+}
+bool laSoChinhPhuong(int a){ //xay dung ham kiem tra so chinh phuong
+    return sqrt(a) == (int)sqrt(a);
+}
+void lietKeSoChinhPhuong(int a[],int n){ // xay dung ham liet ke so chinh phuong
+    for(int i=0;i<n;i++){
+        if(laSoChinhPhuong(a[i])){
+            cout<<a[i]<<" ";
+        }
     }
     cout<<endl;
 }
