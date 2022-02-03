5729c57d44c0ffd5fa73b74a63bf802f86547551
VTSon8 <anhkhongnho88888@gmail.com>
2022-02-03 18:34:22 +0700

    Mang tang dan, giam dan

--- src/Array/Array_v1.cpp
+++ src/Array/Array_v1.cpp
@@ -5,13 +5,17 @@ void nhapMang(int a[],int &n);
 void xuatMang(int a[],int n);
 int phanTuLonNhat(int a[],int n);
 int phanTuNhoNhat(int a[],int n);
+int tangDan(int a[],int n);
+int giamDan(int a[],int n);
 int main(){
     int a[MAX];
     int n;
     nhapMang(a,n);
     xuatMang(a,n);
-    cout<<"Phan tu lon nhat trong mang la: "<<phanTuLonNhat(a,n);
-    cout<<"Phan tu nho nhat trong mang la: "<<phanTuNhoNhat(a,n);
+    cout<<"Phan tu lon nhat trong mang la: "<<phanTuLonNhat(a,n)<<endl;
+    cout<<"Phan tu nho nhat trong mang la: "<<phanTuNhoNhat(a,n)<<endl;
+    tangDan(a,n);
+    giamDan(a,n);
     return 0;
 }
 void nhapMang(int a[],int &n){
@@ -45,4 +49,30 @@ int phanTuNhoNhat(int a[],int n){
         }
     }
     return min;
+}
+int tangDan(int a[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            if(a[j]>a[i]){
+                a[i] = a[i] + a[j];
+                a[j] = a[i] - a[j];
+                a[i] = a[i] - a[j];
+            }
+        }
+    }
+    cout<<"Mang sau khi sap xep tang dan: "<<endl;
+    xuatMang(a,n);
+}
+int giamDan(int a[],int n){
+    for(int i=0;i<n;i++){
+    for(int j=0;j<=i;j++){
+        if(a[j]<a[i]){
+            a[i] = a[i] + a[j];
+            a[j] = a[i] - a[j];
+            a[i] = a[i] - a[j];
+            }
+        }
+    }
+    cout<<"Mang sau khi sap xep giam dan: "<<endl;
+    xuatMang(a,n);
 }
