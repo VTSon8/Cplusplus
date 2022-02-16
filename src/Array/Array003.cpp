cb93ccef4ac900f17c4334f37282aebf38c32302
VTSon8 <anhkhongnho88888@gmail.com>
2022-02-16 21:32:06 +0700

    sap xep tang dan

--- src/Array/Array003.cpp
+++ src/Array/Array003.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void nhapMang(int a[], int &n);
 void xuatMang(int a[], int n);
 int tanSuat(int a[], int n, int x);
+void tangDan(int a[],int n);
 int main(){
     int a[MAX];
     int n;
@@ -12,7 +13,9 @@ int main(){
     nhapMang(a,n);
     xuatMang(a,n);
     cout<<endl;
-    cout<<"so "<<x<<"xuat hien"<<tanSuat(a,n,x);
+    cout<<"so "<<x<<" xuat hien "<<tanSuat(a,n,x)<<" lan"<<endl;
+    tangDan(a,n);
+    xuatMang(a,n);
     return 0;
 }
 void nhapMang(int a[], int &n){
@@ -37,4 +40,24 @@ int tanSuat(int a[], int n, int x){
         }
     }
     return dem;
+}
+// tang dan
+// 3 2 1 2 3
+// i = 0, j = i+1 = 1, 2 3 1 2 3
+// i = 0, j = j++ = 2, 1 3 2 2 3
+// i = 0, j = j++ = 2, (1) 3 2 2 3
+// i = 1, j = i+1 = 2, (1) 2 3 2 3
+// i = 1, j = j++ = 3, (1) (2) 3 2 3
+//...
+//i = 2, j = i+1 = 3, (1) (2) 2 3 3
+void tangDan(int a[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 }
