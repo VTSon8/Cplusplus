f5fa09c6c6e0e57d21537271d3f4a9d70e021f70
VTSon8 <anhkhongnho88888@gmail.com>
2022-02-23 20:38:53 +0700

    khoi tao co tham so

--- src/OOP/Student_v1.cpp
+++ src/OOP/Student_v1.cpp
@@ -13,12 +13,19 @@ class Student{// Animal, People,..
             name = "no name";
             gpa = 3.4;
         };
+        Student(int id, string name, float gpa){// parameter constructor 
+            this->id = id;
+            this->name = name;
+            this->gpa = gpa;
+        }
         print(){
-            cout<<"id: "<<id<<", name: "<<name <<", gpa: "<<gpa;
+            cout<<"id: "<<id<<", name: "<<name <<", gpa: "<<gpa<<endl;
         }
 };
 int main(){
     Student minh;// khởi tạo 1 object là student
+    Student nam = Student(2,"Nam",4.3);// gọi hàm khởi tạo có đối số
     minh.print();// gọi hàm print
+    nam.print();// gọi hàm xuât thông tin của nam
 
 }
