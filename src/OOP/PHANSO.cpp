8b7236182741abaee150d53728edd43ee1bf182b
VTSon8 <anhkhongnho88888@gmail.com>
2022-02-23 22:13:45 +0700

    doc ghi file

--- src/OOP/PHANSO.cpp
+++ src/OOP/PHANSO.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 #include<cmath>
 using namespace std;
 class Phanso{
@@ -23,16 +24,34 @@ class Phanso{
             return mauso;
         }
         print(){
-            cout<<"Tu so: "<<tuso<<" Mau so: "<<mauso<<endl;
-            cout<<"Phan so: "<<(float)tuso/mauso; 
+            cout<<tuso<<"/"<<mauso<<endl;
+        }
+        void readFile(string path){
+            // mo file
+            ifstream input(path);// input
+            // doc file
+            input>>tuso>>mauso;
+            // dong file
+            input.close();
+        }
+        void writeFile(string path){
+            ofstream output(path);// mo file
+            // ghi file
+            output<<tuso<<"/"<<mauso;
+            // dong file
+            output.close();
         }
 };
 int main(){
     Phanso phanSo;
+    string path = "phanso.in";
+    string path2 = "phanso.out";
     phanSo.setTuso(9);
     cout<<phanSo.getTuso()<<endl;
     phanSo.setMauso(8);
     cout<<phanSo.getMauso()<<endl;
+    phanSo.readFile(path);
     phanSo.print();
+    phanSo.writeFile(path2);
     return 0;
 }
