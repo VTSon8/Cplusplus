b0dc492ee7b4b4bce1a7620cf05e7e0ba7cc772b
VTSon8 <anhkhongnho88888@gmail.com>
2022-02-23 21:28:36 +0700

    setter/getter

--- src/OOP/SOPHUC.cpp
+++ src/OOP/SOPHUC.cpp
@@ -9,13 +9,21 @@ class Sophuc{
             phanthuc = 1;
             phanao = 2;
         };
-    print(){
-        cout<<"Phan thuc: "<<phanthuc<<" Phan ao: "<<phanao<<endl;
-        cout<<"So phuc: "<<phanthuc<<" + "<<phanao<<"i";
-    }
+        void setPhanThuc(int phanthuc){
+            this->phanthuc = phanthuc;
+        }
+        int getPhanThuc(){
+            return phanthuc;
+        }
+        print(){
+            cout<<"Phan thuc: "<<phanthuc<<" Phan ao: "<<phanao<<endl;
+            cout<<"So phuc: "<<phanthuc<<" + "<<phanao<<"i";
+        }
 };
 int main(){
     Sophuc soPhuc;
+    soPhuc.setPhanThuc(10);
+    cout<<soPhuc.getPhanThuc();
     soPhuc.print();
     return 0;
 }
