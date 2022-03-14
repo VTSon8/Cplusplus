aa4d5546e2718f0af9fd81ca90f78e753ccc6dae
VTSon8 <anhkhongnho88888@gmail.com>
2022-03-14 21:34:47 +0700

    empty, full Stack

---
+++ src/Structure/Queue.cpp
@@ -0,0 +1,38 @@
+// 1 2 3
+// 5 -->5 4 1 2 3
+// push, front, pop, empty, full
+class Queue{
+    private:
+        int size;
+        int elements[10];
+        const int MAX_SIZE = 10;
+    public:
+        Queue();
+        void push(int element);
+        void pop();
+        bool empty();
+        bool full();
+};
+    Queue::Queue(){
+    size = 0;
+}
+void Queue::push(int element){
+    if(size<MAX_SIZE){
+        size++;
+        elements[0] = element;
+    }
+}
+void Queue::pop(){
+    if(size>0){
+        size--;
+    }
+}
+bool Queue::empty(){
+    return size == 0;
+}
+bool Queue::full(){
+    return size == MAX_SIZE;
+}
+int main(){
+    return 0;
+}
--- src/Structure/Stack.cpp
+++ src/Structure/Stack.cpp
@@ -4,63 +4,63 @@
 #include<iostream>
 using namespace std;
 class Stack{
-    private:
-        int size;
-        int elements[10];
-        const int MAX_SIZE = 10;
-    public:
-        Stack();
-        void push(int element);
-        void pop();
-        void print();
-        bool empty();
-        bool full();
-        int top();
+private:
+    int size;
+    int elements[10];
+    const int MAX_SIZE = 10;
+public:
+    Stack();
+    void push(int element);
+    void pop();
+    void print();
+    bool empty();
+    bool full();
+    int top();
 };
 Stack::Stack(){
-    size = 0;
+size = 0;
 }
 // 1 2 3 4 element
 // element = 4 -->
 void Stack::push(int element){
-    // nếu chưa đầy
-    if(size<MAX_SIZE){
-        size++;
-        elements[size-1] = element;
-    }
-    
+// nếu chưa đầy
+if(size<MAX_SIZE){
+    size++;
+    elements[size-1] = element;
+}
+
 }
 void Stack::pop(){
-    if(size>0){
-        size--;
-    }
+if(size>0){
+    size--;
+}
 }
 bool Stack::empty(){
-    return size == 0;
+return size == 0;
 }
 bool Stack::full(){
-    return size == MAX_SIZE;
+return size == MAX_SIZE;
 }
 int Stack::top(){
-    return elements[size-1];
+return elements[size-1];
 }
 void Stack::print(){
-    for(int i=0;i<size;i++){
-        cout<<elements[i]<<" ";
-    }
+for(int i=0;i<size;i++){
+    cout<<elements[i]<<" ";
+}
 }
 int main(){
-    Stack list;
-    list.push(3);
-    list.push(4);
-    list.push(5);
-    list.push(4);
-    list.push(6);
-    list.pop();
-    list.pop();
-    list.pop();
-    list.print();
-    cout<<list.full()<<" ";
-    cout<<list.top();
-    return 0;
+Stack list;
+list.push(3);
+list.push(4);
+list.push(5);
+list.push(4);
+list.push(6);
+list.pop();
+list.pop();
+list.pop();
+list.print();
+cout<<list.full()<<" ";
+cout<<list.top();
+return 0;
 }
