cf13af4a8d78efa808fc0a69bdf33e40a1037d14
VTSon8 <anhkhongnho88888@gmail.com>
2022-03-14 22:50:33 +0700

    Array

--- src/Structure/Array.cpp
+++ src/Structure/Array.cpp
@@ -1,32 +1,86 @@
 // append(Array) --> thêm
 // remove(int index) ---> xóa
 // update(int index, int value)
-// exist(int value);
+// exist(int value);// value: 4 -->tồn tại: true/false
 // getElement(int index);--> a[3]
+// 1 2 3
+// 3 2 4
+// 1 2 3 3 2 4
+#include<iostream>
+using namespace std;
 class Array{
     private:
         int size;
-        int arrays[10];
+        int elements[10];
         const int MAX_SIZE = 10;
     public:
         Array();
-        void append(int array);
+        int getElement(int index);
+        void setSize(int size){this->size=size;}
+        int getSize(){return size;}
+        void push(int element);
+        Array append(Array other);
         void remove(int index);
+        void print();
+        
 };
 Array::Array(){
     size = 0;
 }
-void Array::append(int array){
+void Array::push(int element){
     if(size<MAX_SIZE){
         size++;
-        arrays[size-1] = array;
+        elements[size-1] = element;
     }
 }
+// 1 2 3 4 5 6
+// 1 2 4 5 6
 void Array::remove(int index){
-    if(size>0){
+    if(index<=size){
+        for(int i=index;i<size;i++){
+            elements[i] = elements[i+1];
+        }
         size--;
     }
 }
+int Array::getElement(int index){
+    return elements[index];
+}
+void Array::print(){
+    for(int i=0;i<size;i++){
+        cout<<elements[i]<<" ";
+    }
+}
+//current 2 3 4
+// other 6 7 8
+// result 2 3 4| 6 7 8
+Array Array::append(Array other){
+    Array result;
+    // thay doi size
+    // result.setSize(size+other.getSize());
+    // them mang minh dang xet
+    for(int i=0;i<size;i++){
+        result.push(elements[i]);
+    }
+    // them mang append
+    for(int i=0;i<other.getSize();i++){
+        result.push(other.getElement(i));
+    }
+    return result;
+}
 int main(){
+    Array list;
+    Array list2;
+    // int index;
+    // cin>>index;
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
+    list.push(5);
+    list2.push(3);
+    list2.push(7);
+    // list.remove(index);
+    list.append(list2).print();
     return 0;
 }
